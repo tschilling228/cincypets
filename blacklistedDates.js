164ad0f83fc2f6e5baa78356af260744337a75da
Taryn Schilling <44912184+tschilling228@users.noreply.github.com>
2019-03-30 14:44:32 -0400

    Update blacklistedDates.js

--- blacklistedDates.js
+++ blacklistedDates.js
@@ -1,2 +1,2 @@
 // Format: ""01/04/2019", "02/04/2019", "01/05/2019", ... = Apr 1st, Apr 2nd, May 1st
-const blacklistedDates = ["01/04/2019", "08/04/2019"];
+const blacklistedDates = ["01/04/2019"];
